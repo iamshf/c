c46aa94ef874d800f1e9d7724bf2b5b5a43616ef
iamshf <haofengsheng@gmail.com>
2018-07-01 15:48:24 +0800

    fix quick sort bug

--- sorting_algorithm/quicksort.c
+++ sorting_algorithm/quicksort.c
@@ -4,9 +4,8 @@
 
 #include "sort_public.c"
 
-/*快速排序不稳定，当有相同元素时，排序会出错*/
 int getrand(int max){
-    if(max == 0){
+    if(max == 0 || max == 1){
         return 0;
     }
     else {
@@ -15,20 +14,21 @@ int getrand(int max){
     }
 }
 void quicksort(int *plist,int length, int start, int end){
-    if(start <  end){
+    if(start <  end && length > 0){
         int pivotIndex = getrand(length) + start;
         swap(plist, pivotIndex, end);
         int k = start;
         int i;
-        for(i = start; i <= end; i++){
-            if(*(plist + i) <  *(plist + end)){
+        for(i = start; i < end; i++){
+            if(*(plist + i) <=  *(plist + end)){
                 swap(plist, i, k);
                 k++;
             }
         }
         swap(plist, k, end);
-        quicksort(plist, k, 0, k - 1);
-        quicksort(plist, length -1 - k, k + 1, end);
+
+        quicksort(plist, k - start, start, k - 1);
+        quicksort(plist, end -k, k + 1, end);
     }
 }
 void main(){
--- sorting_algorithm/sort_public.c
+++ sorting_algorithm/sort_public.c
@@ -1,10 +1,13 @@
 void swap(int *plist, int i, int j){
-    int tmp = *(plist + i);
-    *(plist + i) = *(plist + j);
-    *(plist + j) = tmp;
+    if(i != j){
+        int tmp = *(plist + i);
+        *(plist + i) = *(plist + j);
+        *(plist + j) = tmp;
+    }
 }
 void output(int *plist, int length){
     int i;
+    printf("after sort result:");
     for(i = 0; i < length; i++){
         printf("%d,", *(plist + i));
     }
